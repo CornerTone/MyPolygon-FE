2469c98805fde77d39a898b482349dd42f578f78
soyyyyy <da5868@naver.com>
2024-02-02 02:38:36 +0900

    feat. 회원가입, 로그인, 첫 페이지, 메인, 집중기록 피그마 코드 변환

---
+++ selectpolygon/src/pages/Signin/SignPageStyle.js
@@ -0,0 +1,244 @@
+import styled from "@emotion/styled";
+
+export const RootWrapperNaN = styled.div`
+	min-height: 100vh;
+	background: rgb(255, 255, 255);
+	background-repeat: no-repeat;
+	background-size: contain;
+	background-position: center;
+	position: relative;
+`;
+
+export const Rectangle3 = styled.div`
+	width: 1px;
+	height: 3px;
+	background: rgb(217, 217, 217);
+	background-repeat: no-repeat;
+	background-size: contain;
+	background-position: center;
+	position: absolute;
+	left: 386px;
+	top: 0px;
+`;
+
+export const Rectangle37 = styled.div`
+	width: 401px;
+	height: 90px;
+	background: rgb(245, 239, 231);
+	background-repeat: no-repeat;
+	background-size: contain;
+	background-position: center;
+	border: solid 0px black;
+	border-radius: 20px;
+	position: absolute;
+	left: -20px;
+	top: 151px;
+	filter: blur(4px);
+`;
+
+export const Typography = styled.div`
+	display: flex;
+	justify-content: flex-start;
+	flex-direction: row;
+	align-items: flex-start;
+	gap: 10px;
+	box-sizing: border-box;
+	position: absolute;
+	left: 147px;
+	top: 182px;
+`;
+
+export const Typography_0001 = styled.span`
+	color: rgb(51, 51, 51);
+	text-overflow: ellipsis;
+	font-size: 18px;
+	font-family: Poppins, sans-serif;
+	font-weight: bold;
+	text-align: center;
+`;
+
+export const Typography_0002 = styled.div`
+	display: flex;
+	justify-content: flex-start;
+	flex-direction: row;
+	align-items: flex-start;
+	gap: 10px;
+	box-sizing: border-box;
+	position: absolute;
+	left: 122px;
+	top: 169px;
+`;
+
+export const Group13 = styled.div`
+	width: 76px;
+	height: 28px;
+	position: absolute;
+	left: 46px;
+	top: 359px;
+`;
+
+export const Rectangle23 = styled.div`
+	width: 76px;
+	height: 28px;
+	box-shadow: 0px 4px 4px  rgba(0, 0, 0, 0.25);
+	background: rgb(235, 186, 113);
+	background-repeat: no-repeat;
+	background-size: contain;
+	background-position: center;
+	border-radius: 10px;
+	position: absolute;
+	left: 0px;
+	top: 0px;
+`;
+
+export const NaN_0002 = styled.span`
+	color: black;
+	text-overflow: ellipsis;
+	font-size: 18px;
+	font-family: Inter, sans-serif;
+	font-weight: bold;
+	text-align: center;
+	width: 59px;
+	position: absolute;
+	left: 9px;
+	top: 3px;
+`;
+
+export const Group14 = styled.div`
+	width: 83px;
+	height: 28px;
+	position: absolute;
+	left: 43px;
+	top: 418px;
+`;
+
+export const Rectangle22 = styled.div`
+	width: 76px;
+	height: 28px;
+	box-shadow: 0px 4px 4px  rgba(0, 0, 0, 0.25);
+	background: rgb(188, 220, 99);
+	background-repeat: no-repeat;
+	background-size: contain;
+	background-position: center;
+	border-radius: 10px;
+	position: absolute;
+	left: 3px;
+	top: 0px;
+`;
+
+export const NaN_0003 = styled.span`
+	color: black;
+	text-overflow: ellipsis;
+	font-size: 18px;
+	font-family: Inter, sans-serif;
+	font-weight: bold;
+	text-align: center;
+	width: 83px;
+	position: absolute;
+	left: 0px;
+	top: 3px;
+`;
+
+export const Group15 = styled.div`
+	width: 79px;
+	height: 28px;
+	position: absolute;
+	left: 45px;
+	top: 474px;
+`;
+
+export const Rectangle22_0001 = styled.div`
+	width: 76px;
+	height: 28px;
+	box-shadow: 0px 4px 4px  rgba(0, 0, 0, 0.25);
+	background: rgb(133, 187, 226);
+	background-repeat: no-repeat;
+	background-size: contain;
+	background-position: center;
+	border-radius: 10px;
+	position: absolute;
+	left: 1px;
+	top: 0px;
+`;
+
+export const NaN_0004 = styled.span`
+	color: black;
+	text-overflow: ellipsis;
+	font-size: 18px;
+	font-family: Inter, sans-serif;
+	font-weight: bold;
+	text-align: center;
+	width: 79px;
+	position: absolute;
+	left: 0px;
+	top: 3px;
+`;
+
+export const Group16 = styled.div`
+	width: 85px;
+	height: 28px;
+	position: absolute;
+	left: 138px;
+	top: 574px;
+`;
+
+export const Rectangle22_0002 = styled.div`
+	width: 85px;
+	height: 28px;
+	box-shadow: 0px 4px 4px  rgba(0, 0, 0, 0.25);
+	background: rgb(243, 228, 208);
+	background-repeat: no-repeat;
+	background-size: contain;
+	background-position: center;
+	border-radius: 10px;
+	position: absolute;
+	left: 0px;
+	top: 0px;
+`;
+
+export const NaN_0005 = styled.span`
+	color: black;
+	text-overflow: ellipsis;
+	font-size: 18px;
+	font-family: Inter, sans-serif;
+	font-weight: bold;
+	text-align: center;
+	width: 85px;
+	position: absolute;
+	left: 0px;
+	top: 3px;
+`;
+
+export const Rectangle49 = styled.svg`
+	width: 175px;
+	height: 28px;
+	position: absolute;
+	left: 144px;
+	top: 359px;
+`;
+
+export const Rectangle50 = styled.svg`
+	width: 175px;
+	height: 28px;
+	position: absolute;
+	left: 144px;
+	top: 418px;
+`;
+
+export const Rectangle51 = styled.svg`
+	width: 175px;
+	height: 28px;
+	position: absolute;
+	left: 144px;
+	top: 474px;
+`;
+
+export const Vector = styled.svg`
+	width: 20px;
+	height: 15px;
+	position: absolute;
+	left: 16px;
+	top: 22px;
+	right: 324px;
+	bottom: 763px;
+`;
