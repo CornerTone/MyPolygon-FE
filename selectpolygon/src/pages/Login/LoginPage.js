5d58cb6dde7c0ec0b284bcafb1395df78dd01dc0
정지민 <141540563+sunflwwer@users.noreply.github.com>
2024-02-04 17:07:08 +0900

    로그인 수정

--- selectpolygon/src/pages/Login/LoginPage.js
+++ selectpolygon/src/pages/Login/LoginPage.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import * as L from "./LoginPageStyle";
 import * as S from "../Signin/SignPageStyle";
-import { Link } from "react-router-dom";
 import { goBack } from "../../components/backNavigation";
 
 export function LoginPage() {
@@ -10,6 +10,11 @@ export function LoginPage() {
     nickname: "",
     password: "",
   });
+
+  const [loginError, setLoginError] = useState(false); // 로그인 실패 상태
+  const [showModal, setShowModal] = useState(false); // 모달 창 표시 여부
+
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({
@@ -19,7 +24,6 @@ export function LoginPage() {
   };
 
   const handleSubmit = async (e) => {
-    console.log("in");
     e.preventDefault();
 
     try {
@@ -27,27 +31,40 @@ export function LoginPage() {
         "http://localhost:3001/api/auth/login",
         formData
       );
-      console.log(response.data); // 서버의 응답 데이터를 콘솔에 출력
+
+      if (response.data.success) {
+        console.log("로그인 성공");
+        // 로그인 성공 시에 추가적인 작업 수행
+        // ...
+
+        // 페이지 이동
+        navigate("/main");
+      } else {
+        console.log("로그인 실패");
+        // 실패 시에 상태 업데이트
+        setLoginError(true);
+        // 모달 창 표시
+        setShowModal(true);
+      }
     } catch (error) {
       console.error("데이터 전송 중 오류 발생:", error);
+      // 오류 시에 상태 업데이트
+      setLoginError(true);
+      // 모달 창 표시
+      setShowModal(true);
     }
+  };
+
+  const closeModal = () => {
+    // 모달 창 닫기
+    setShowModal(false);
+    // 실패 상태 초기화
+    setLoginError(false);
   };
 
   return (
     <L.RootWrapperNaN>
-      <L.Rectangle3 />
       <form onSubmit={handleSubmit}>
-        {" "}
-        {/* 폼 제출 이벤트를 처리할 form 요소 추가 */}
-        <L.Rectangle39 />
-        <S.Group16>
-          <S.Rectangle22_0002 />
-          <button type="submit" onClick={handleSubmit}>
-            <Link to="/main">
-              <S.NaN_0005>로그인</S.NaN_0005>
-            </Link>
-          </button>
-        </S.Group16>
         <L.Group12_0001>
           <L.Rectangle23 />
           <L.NaN_0004>아이디</L.NaN_0004>
@@ -68,7 +85,34 @@ export function LoginPage() {
           value={formData.password}
           onChange={handleChange}
         />
+        <S.Rectangle37 />
+        <S.Typography>
+          <S.Typography_0001>로그인</S.Typography_0001>
+        </S.Typography>
+        <S.Group16>
+          <S.Rectangle22_0002 />
+          <div onClick={handleSubmit}>
+            <S.NaN_0005>로그인</S.NaN_0005>
+          </div>
+        </S.Group16>
+        <S.Vector xmlns="http://www.w3.org/2000/svg" onClick={goBack}>
+          <path
+            fill="rgba(0, 0, 0, 0.64)"
+            d="M0 7.50001C0 7.74737 0.118471 7.97494 0.333872 8.16294L7.4852 14.723C7.7006 14.911 7.92677 15 8.18525 15C8.71299 15 9.13302 14.6439 9.13302 14.1491C9.13302 13.9117 9.03609 13.6742 8.86377 13.5257L6.45127 11.2698L2.18633 7.6979L1.96015 8.18273L5.42811 8.38062L19.0523 8.38062C19.6123 8.38062 20 8.01452 20 7.50001C20 6.9855 19.6123 6.6194 19.0523 6.6194L5.42811 6.6194L1.96015 6.81729L2.18633 7.31202L6.45127 3.73022L8.86377 1.47427C9.03609 1.31596 9.13302 1.08839 9.13302 0.850923C9.13302 0.356201 8.71299 0 8.18525 0C7.92677 0 7.7006 0.0791558 7.46365 0.296834L0.333872 6.83708C0.118471 7.02507 0 7.25265 0 7.50001Z"
+          />
+        </S.Vector>
       </form>
+
+      {loginError && showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <p>로그인에 실패했습니다. 다시 시도하세요.</p>
+          </div>
+        </div>
+      )}
     </L.RootWrapperNaN>
   );
 }
