356cb489678c1d331c5e1027bf7698d51619683f
정지민 <141540563+sunflwwer@users.noreply.github.com>
2024-02-05 16:14:07 +0900

    집중기록 디자인

---
+++ selectpolygon/src/pages/Select/PolygonSelectStyle.js
@@ -0,0 +1,221 @@
+import styled from "@emotion/styled";
+
+export const RootWrapperNaN = styled.div`
+  min-height: 100vh;
+  background: rgb(255, 255, 255);
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  position: relative;
+`;
+
+export const Group17 = styled.div`
+  width: 334px;
+  height: 432px;
+  position: absolute;
+  left: 14px;
+  top: 222px;
+`;
+
+export const Rectangle22 = styled.div`
+  width: 334px;
+  height: 432px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  background: rgb(255, 255, 255);
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  border: solid 0px black;
+  border-radius: 20px;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  backdrop-filter: blur(4px);
+`;
+
+export const Group7 = styled.div`
+  width: 150px;
+  height: 150px;
+  position: absolute;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  left: 12px;
+  top: 12px;
+`;
+
+export const Frame5 = styled.div`
+  width: 150px;
+  height: 150px;
+  overflow: hidden;
+  background: rgb(79, 112, 156);
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  border-radius: 10px;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+`;
+
+export const Polygon1 = styled.svg`
+  width: 110px;
+  height: 110px;
+  position: absolute;
+  left: calc((calc((50% + 0px)) - 55px));
+  top: calc((calc((50% + 13px)) - 55px));
+`;
+
+export const Group8 = styled.div`
+  width: 150px;
+  height: 150px;
+  position: absolute;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  left: 91px;
+  top: 231px;
+`;
+
+export const Star25 = styled.svg`
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  left: 25px;
+  top: 29px;
+  right: 25px;
+  bottom: 21px;
+`;
+
+export const Frame5_0002 = styled.div`
+  overflow: hidden;
+  background: rgb(79, 112, 156);
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  border-radius: 10px;
+  position: absolute;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  left: 171px;
+  top: 12px;
+  right: 13px;
+  bottom: 270px;
+`;
+
+export const Rectangle40 = styled.div`
+  width: 80px;
+  height: 80px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  background: rgb(217, 217, 217);
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  position: absolute;
+  left: 35px;
+  top: 35px;
+`;
+
+export const Group10 = styled.div`
+  width: 55px;
+  height: 25px;
+  position: absolute;
+  left: 226px;
+  top: 174px;
+`;
+
+export const Rectangle23 = styled.div`
+  width: 55px;
+  height: 25px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  background: rgb(235, 186, 113);
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  border-radius: 10px;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+`;
+
+export const NaN_0002 = styled.span`
+  color: black;
+  text-overflow: ellipsis;
+  font-size: 14px;
+  font-family: Inter, sans-serif;
+  font-weight: 400;
+  text-align: center;
+  width: 43px;
+  position: absolute;
+  left: 7px;
+  top: 4px;
+`;
+
+export const Group10_0001 = styled.div`
+  width: 55px;
+  height: 25px;
+  position: absolute;
+  left: 139px;
+  top: 394px;
+`;
+
+export const Rectangle25 = styled.div`
+  width: 55px;
+  height: 25px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  background: rgb(133, 187, 226);
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  border-radius: 10px;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+`;
+
+export const NaN_0003 = styled.span`
+  color: black;
+  text-overflow: ellipsis;
+  font-size: 14px;
+  font-family: Inter, sans-serif;
+  font-weight: 400;
+  text-align: center;
+  width: 43px;
+  position: absolute;
+  left: 6px;
+  top: 4px;
+`;
+
+export const Group11 = styled.div`
+  width: 55px;
+  height: 25px;
+  position: absolute;
+  left: 58px;
+  top: 177px;
+`;
+
+export const Rectangle23_0001 = styled.div`
+  width: 55px;
+  height: 25px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  background: rgb(243, 228, 208);
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  border-radius: 10px;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+`;
+
+export const Group55 = styled.div`
+  width: 401px;
+  height: 90px;
+  position: absolute;
+  left: -21px;
+  top: 114px;
+`;
+
+export const Rectangle38 = styled.div`
+  width: 401px;
+  height: 90px;
+  background: rgb(245, 239, 231);
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position;
+  `;
